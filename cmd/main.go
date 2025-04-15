869b9053c1a4df7e0a520e4a417dbedbf773e7bd
Ivan Vasilev <20002mc@gmail.com>
2025-04-16 00:42:52 +0300

    parse .lua args as path

--- cmd/main.go
+++ cmd/main.go
@@ -20,6 +20,12 @@ func main() {
 func parseConfig() config.Config {
 	script := "demo"
 	if len(os.Args) >= 2 {
+		arg := os.Args[1]
+		if strings.HasSuffix(arg, ".lua") {
+			return config.Config{
+				Script: arg,
+			}
+		}
 		script = strings.TrimSuffix(os.Args[1], ".lua")
 	}
 	script = "demo/" + script + ".lua"
