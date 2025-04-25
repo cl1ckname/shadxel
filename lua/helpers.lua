8c8c98529c89edc3b635e61634a1481ea9f61a2b
Ivan Vasilev <20002mc@gmail.com>
2025-04-25 20:06:44 +0300

    draw the bird

--- lua/helpers.lua
+++ lua/helpers.lua
@@ -1,3 +1,9 @@
+---@class Point
+---@field x integer
+---@field y integer
+---@field z integer
+
+
 function voxel(r, g, b, visible)
 	local v = r * 0x1000000 + g * 0x10000 + b * 0x100
 	if visible then
@@ -20,11 +26,31 @@ function isin(x, a, b)
 	return a < x and x < b
 end
 
+---@param x integer
+---@param y integer
+---@param z integer
+---@return Point
+function point(x, y, z)
+	return { x = x, y = y, z = z }
+end
+
+---@param p Point
+---@param p1 Point
+---@param p2 Point
+---@return boolean
+function isinbox(p, p1, p2)
+	return p1.x < p.x and p.x < p2.x and
+		p1.y < p.y and p.y < p2.y and
+		p1.z < p.z and p.z < p2.z
+end
+
 return {
 	voxel = voxel,
 	color = color,
 	isin = isin,
 	null = null,
+	point = point,
+	isinbox = isinbox,
 
 	WHITE = color(255, 255, 255),
 	BLACK = color(0, 0, 0),
--- lua/template.lua
+++ lua/template.lua
@@ -1,57 +1,5 @@
--- @diagnostic disable: undefined-global
 h = require("helpers")
 
-local eye = h.color(64,32,64)
-local nose = h.color(200, 160, 64)
-local wing = h.color(170, 40, 50)
-local body = h.color(220, 30, 80)
-local hat = h.color(64, 32, 64)
-
-local bw = 12
-local bh = 10
-
-local function iswing(x,y,z)
-	return h.isin(x, -1, 1) and h.isin(z, -3, bw-3) and h.isin(y, -5, 1)
-end
-
-local function iswing2(x,y,z)
-	return h.isin(x, -1, 1) and h.isin(z, -5, bw-5) and h.isin(y, -7, -1)
-end
-
 function Draw(x, y, z, t)
-
-	if h.isin(x, -bw, bw) and h.isin(y, 1, 8) and h.isin(z, 8, bw) then
-		return eye
-	end
-	if h.isin(x, -bw, bw) and h.isin(y, 2, 8) and h.isin(z, 3, bw) then
-		return eye
-	end
-	if h.isin(x, -3, 3) and h.isin(y, bh-2, bh+4) and h.isin(z, bw-5, bw+1) then
-		return hat
-	end
-	if h.isin(x, -2, 2) and h.isin(y, bh-2, bh+5) and h.isin(z, bw-7, bw-1) then
-		return hat
-	end
-	if h.isin(z, bw-1, bw+2) and h.isin(x, -2, 2) and h.isin(y, 0, 4) then
-		return nose
-	end
-	if h.isin(x, bw-1,bw+1) and h.isin(z, 0, bw) and h.isin(y, -5, 1) then
-		return wing
-	end
-	if iswing(x-bw, y,z) or iswing2(x-bw, y, z) or iswing(x+bw, y, z) or iswing2(x+bw, y, z) then
-		return wing
-	end
-	if x < bw and x > -bw and y < bh and y > -bh and z < bw and z > -bw then
-		return body
-	end
-	if h.isin(y, -14, -bh+1) and (h.isin(x, 2, 8) or h.isin(x, -8, -2)) and (h.isin(z, 2, 8) or h.isin(z, -8, -2)) then
-		return h.BLACK
-	end
-	if h.isin(x, -bw-1, bw+1) and y == 6 and z == 8 then
-		return h.WHITE
-	end
-	if x < bw+1 and x > -bw-1 and y <= 6 and y >=4 and z <= 8 and z >=6 then
-		return h.BLACK
-	end
 	return h.null
 end
